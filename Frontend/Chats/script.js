b16b1c29c4606c980e6094e732d307ee91ff88a7
wrjodjana <141975732+wrjodjana@users.noreply.github.com>
2023-12-03 00:07:22 -0600

    fixed chat.html

---
+++ Frontend/Chats/script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const restaurants = ["KFC", "Hokben", "Carrefour"];
+  const restaurantList = document.getElementById("restaurant-list");
+
+  // Populate restaurant list
+  restaurants.forEach((restaurant, index) => {
+    const div = document.createElement("div");
+    div.textContent = restaurant;
+    div.addEventListener("click", () => selectRestaurant(restaurant));
+    restaurantList.appendChild(div);
+  });
+
+  document.getElementById("send-btn").addEventListener("click", sendMessage);
+});
+
+function selectRestaurant(restaurant) {
+  const chatWindow = document.getElementById("chat-window");
+  chatWindow.innerHTML = `Chatting with ${restaurant}`;
+}
+
+function sendMessage() {
+  const input = document.getElementById("chat-input");
+  const message = input.value.trim();
+  if (message) {
+    const chatWindow = document.getElementById("chat-window");
+    const messageDiv = document.createElement("div");
+    messageDiv.textContent = message;
+    chatWindow.appendChild(messageDiv); // Appends new div for each message
+    chatWindow.scrollTop = chatWindow.scrollHeight; // Auto-scrolls to the bottom
+    input.value = "";
+  }
+}
